935c98c199eb27d326352e3df9036959ec317af1
Nils Butenschön <moin@nilsbutenschoen.de>
2015-03-07 09:35:38 +0100

    Rework routing for renamed folders

--- js/app.js
+++ js/app.js
@@ -14,15 +14,15 @@ nilsApp.config(['$routeProvider', function ($routeProvider) {
     'use strict';
     $routeProvider
     // Home
-        .when("/", {templateUrl: "_partials/home.html", controller: "PageCtrl"})
+        .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
     // Pages
-        .when("/music", {templateUrl: "_partials/music.html", controller: "PageCtrl"})
-        .when("/vita", {templateUrl: "_partials/vita.html", controller: "VitaPageCtrl"})
-        .when("/contact", {templateUrl: "_partials/contact.html", controller: "PageCtrl"})
+        .when("/music", {templateUrl: "partials/music.html", controller: "PageCtrl"})
+        .when("/vita", {templateUrl: "partials/vita.html", controller: "VitaPageCtrl"})
+        .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
     // Blog
-        .when("/blog", {templateUrl: "_partials/blog.html", controller: "BlogCtrl"})
-        .when("/blog/posts/:postSlug", {templateUrl: "_partials/blog_post.html", controller: "BlogPostCtrl"})
-        .when("/blog/tags/:tagName", {templateUrl: "_partials/category_list.html", controller: "CatListCtrl"})
+        .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
+        .when("/blog/posts/:postSlug", {templateUrl: "partials/blog_post.html", controller: "BlogPostCtrl"})
+        .when("/blog/tags/:tagName", {templateUrl: "partials/category_list.html", controller: "CatListCtrl"})
     // else redirect to Home
-        .otherwise({templateUrl: "_partials/404.html", controller: "PageCtrl"});
+        .otherwise({templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
